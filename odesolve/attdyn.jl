6a3284f2f7b1182fa851b74f08305f38a2007517
leo-collins <leocllins511@gmail.com>
2023-06-29 11:25:06 +0100

    graphs

--- odesolve/attdyn.jl
+++ odesolve/attdyn.jl
@@ -18,10 +18,10 @@ end
 
 tspan = (0.0, 100)
 p = [-1.1, -2.0, 1.0, 1.0, 1.0, 0.01, 0.3]
-u₀ = [0.0, 1.0, 0.1]
+u₀ = [0.0, 1.0, 1.0]
 
 prob = ODEProblem(opinion!, u₀, tspan, p)
 sol = solve(prob, AutoTsit5(Rosenbrock23()), reltol=1e-8, abstol=1e-8)
 # sol = solve(prob)
 
-scene = plot(sol, idxs=(1, 2, 3), title="u₀=[$(u₀[1]), $(u₀[2]), $(u₀[3])]", xlabel='x', ylabel='y', zlabel='A')
+scene = plot(sol, idxs=(1, 2), title="u₀=[$(u₀[1]), $(u₀[2]), $(u₀[3])]", xlabel='x', ylabel='y', zlabel='A', size=(1000, 1000), lw=2)
